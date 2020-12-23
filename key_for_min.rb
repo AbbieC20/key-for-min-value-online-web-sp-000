fb5e26f3ca58d4a0d6ddd20dc45318f3eadb0771
remote-torrent-6747 <abbyc@sky.com>
2020-12-23 11:28:39 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,8 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  name_hash.each do |key, value|
+    puts "#{key}: #{value}"
+  end
 end
 
 #return the key, not the value
-#return the smallest 
+#return the smallest
