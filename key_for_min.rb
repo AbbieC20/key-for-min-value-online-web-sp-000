e85988039d5c3273c402eb2983ea0d4b6e5ef04c
remote-torrent-6747 <abbyc@sky.com>
2020-12-23 12:33:48 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,21 +1,5 @@
 
-# def key_for_min_value(name_hash)
-#   smallest_value = ""
-#   name_hash.each do |key, value|
-#     if value == 1 || value == 10
-#     smallest_value = key
-#   end
-# end
-#
-# smallest_value
-#
-# end
-
 def key_for_min_value(name_hash)
-  if name_hash.empty?
-    return nil
-  end
-
   smallest_value = nil
   matching_key = nil
   name_hash.each do |key, value|
